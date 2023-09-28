841e506406d22be6fa34b7eefea225184766fac9
Alex Gorischek <alex.gorischek@gmail.com>
2023-09-27 19:32:16 -0700

    Move type declarations to root

--- src/types/matchers.ts
+++ src/types/matchers.ts
@@ -2,12 +2,14 @@ import { Options } from '../index.js';
 import { Generator } from './generation.js';
 import { Result } from './results.js';
 
+export type ToGenerateMatcher = (
+  received: Generator,
+  expected: string,
+  count?: number,
+) => Promise<Result>;
+
 export type Matchers = {
-  toGenerate: (
-    received: Generator,
-    expected: string,
-    count?: number,
-  ) => Promise<Result>;
+  toGenerate: ToGenerateMatcher;
 };
 
 export type MatchersFactory<T> = (backend: T, options?: Options) => Matchers;
